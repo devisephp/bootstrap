ede7f6779ed3be5b4bf2065a6edcc547e6ce0307
i-am-not-a-robot <i-am-not-a-robot@lbm.co>
2015-02-17 00:02:34 -0500

    updating devise bootstrap

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2ad8d23fc2b9ebc4dd587a7fe7d7ba6d::getLoader();
+return ComposerAutoloaderInitaad33f3ad03659f6efdaff5d3276b2af::getLoader();
